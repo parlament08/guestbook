4b807ed83bba3e4503416670897ee47dddacb3f9
Yury Tv <iuratv86@mail.ru>
2021-01-06 22:36:25 +0200

    Added ConferenceController

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(REquest $request): Response
+    {
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+        /*
+        return $this->render('conference/index.html.twig', [
+            'controller_name' => 'ConferenceController',
+        ]);
+        */
+        
+        return new Response(<<<EOF
+            <html>
+                <body>
+                $greet;
+                    <img src="/images/under-construction.gif" />
+                </body>
+            </html>
+            EOF
+        );
+    }
+}
