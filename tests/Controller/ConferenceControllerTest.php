64884466a4893216a5367daa8d58d8001bfc8d29
Yury Tv <iuratv86@mail.ru>
2021-01-08 21:47:40 +0200

    Added test for Form submit in the functional testing

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -27,6 +27,22 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertPageTitleContains('Chisinau');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Chisinau 2021');
-        $this->assertSelectorExists('div:contains("There are 6 comments")');
+        $this->assertSelectorExists('div:contains("There are")');
     }
+    
+    public function testCommentSubmission()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/conference/chisinau-2021');
+        $client->submitForm('Submit', [
+            'comment_form[author]' => 'Fabien',
+            'comment_form[text]' => 'Some feedback from an automated functional test',
+            'comment_form[email]' => 'me@automat.ed',
+            'comment_form[photo]' => dirname(__DIR__, 2).'/public/images/under-construction.gif',
+        ]);
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorExists('div:contains("There are")');
+    }
+    
 }
