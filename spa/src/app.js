98972b50f20b5a497b21fd9277e9734b8e969537
Yury Tv <iuratv86@mail.ru>
2021-01-11 21:13:11 +0200

    Created first SPA App using Preact library - 1 page with text Hello World

---
+++ spa/src/app.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import {h, render} from 'preact';
+
+function App() {
+    return (
+        <div>
+            Hello world!
+        </div>
+    )
+}
+
+render(<App />, document.getElementById('app'));
