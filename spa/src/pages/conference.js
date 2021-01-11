653b08167f4969f8258c8d62cf38005c9d03df9a
Yury Tv <iuratv86@mail.ru>
2021-01-11 21:31:18 +0200

    Step 27.5 - Added styles for SPA

---
+++ spa/src/pages/conference.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import {h} from 'preact';
+
+export default function Conference() {
+    return (
+        <div>Conference</div>
+    );
+};
+
+
+
